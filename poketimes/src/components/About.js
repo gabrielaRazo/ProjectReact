00a2d183a788ad3c2e47d8e58b47e01d3affcd11
gabrielaRazo <gabrielarazoroldan@gmail.com>
2020-03-26 22:08:02 -0600

    We added a hoc file that contains an UI Component called Rainbow that change the color of the the color of the about content

--- poketimes/src/components/About.js
+++ poketimes/src/components/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Rainbow from "../hoc/Rainbow";
 const About = () => {
   return (
     <div>
@@ -16,4 +16,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Rainbow(About);
